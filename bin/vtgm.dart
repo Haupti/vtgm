75021c6237edc74cf35ba0bc732e76b25aad6ef7
Marwin Acker <marwin.extern@funk.net>
2024-02-07 17:38:52 +0100

    initial commit

---
+++ bin/vtgm.dart
@@ -0,0 +1,5 @@
+import 'package:vtgm/vtgm.dart' as vtgm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vtgm.calculate()}!');
+}
---
+++ test/vtgm_test.dart
@@ -0,0 +1,8 @@
+import 'package:vtgm/vtgm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
