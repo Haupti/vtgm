4a1a8e95b97d29aeb2afaac536944625f70df394
marwin <marwin.ack@googlemail.com>
2024-02-09 01:02:01 +0100

    add h1 to start page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,6 +14,7 @@ void homePage(HttpRequest request, SsrResponse response) {
   Style checkedStyle = Style(textAlign: "center");
   List<Person> persons = getPeople();
   RootPage page = basePage([
+    H1(text: "Statistics", style: Style(fontSize: "24px")),
     Table(rows: [
       TableRow(tableData: [TableHeader(text: "prepared trainig?",style: headerStyle), TableHeader(text: "name",style: headerStyle)]),
       ...persons.map((Person p) =>
