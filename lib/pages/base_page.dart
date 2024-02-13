39965100a6f99a67b7bb361a603c0859066d6528
Marwin Acker <marwin.extern@funk.net>
2024-02-13 15:51:11 +0100

    use specrecss styling for home page table

--- lib/pages/base_page.dart
+++ lib/pages/base_page.dart
@@ -20,31 +20,47 @@ Component navBar() {
     justifyContent: "start",
   );
 
-  var home =  Anchor(href:"/",text:"home", style: aStyle);
-  var personCheck =  Anchor(href:"/person/update",text:"manage", style: aStyle);
-  var personDelete =  Anchor(href:"/person/delete",text:"delete person", style: aStyle);
-  var personAdd =  Anchor(href:"/person/add",text:"add person", style: aStyle);
+  var home = Anchor(href: "/", text: "home", style: aStyle);
+  var personCheck = Anchor(href: "/person/update", text: "manage", style: aStyle);
+  var personDelete = Anchor(href: "/person/delete", text: "delete person", style: aStyle);
+  var personAdd = Anchor(href: "/person/add", text: "add person", style: aStyle);
   AuthRole currentRole = getCurrentAuthorizedUserRole();
   var visibleAnchors = [];
-  if(currentRole == AuthRole.basic){
+  if (currentRole == AuthRole.basic) {
     visibleAnchors = [home];
-  } else if( currentRole == AuthRole.mod){
+  } else if (currentRole == AuthRole.mod) {
     visibleAnchors = [home, personCheck];
-  } else if( currentRole == AuthRole.admin){
+  } else if (currentRole == AuthRole.admin) {
     visibleAnchors = [home, personCheck, personDelete, personAdd];
   }
-  return Div(children: [
-    Paragraph(text: "H5 TGM VB"),
-    ...visibleAnchors
-  ], style: wrapperStyle);
+  return Div(children: [Paragraph(text: "H5 TGM VB"), ...visibleAnchors], style: wrapperStyle);
 }
 
-RootPage basePage(List<Component> children){
-  Style style = Style(
-    padding: "10px 10px 0 10px"
-  );
-  return RootPage(title:"VTGM", elems:[
-    navBar(),
-    Div(children: children, style: style)
-  ]);
+String vtgmRoot(String title, String content) {
+  return """
+    <!DOCTYPE HTML>
+    <html lang="en">
+      <head>
+        <META charset="UTF-8">
+        <META name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> $title </title>
+        <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css">
+        <style>
+        </style>
+        </head>
+      <body>
+        ${navBar().render()}
+          <div style="margin: auto;">
+            $content
+          </div>
+      <script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous"></script>
+      </body>
+    </html>
+  """;
+}
+
+RootPage basePage(List<Component> children) {
+  Style style = Style(padding: "10px 10px 0 10px");
+  return RootPage(title: "VTGM", elems: [Div(children: children, style: style)], customRoot: vtgmRoot);
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,30 +5,29 @@ import 'package:ssr/ssr.dart';
 import 'package:ssr/html.dart';
 
 void homePage(SsrRequest request, SsrResponse response) {
-  Style headerStyle = Style(color: "white", textAlign: "left", padding: "15px", border: "1px solid #54585d", backgroundColor: "#54585d");
-  Style dataStyle = Style(textAlign: "left", padding: "5px 15px");
-  Style checkedStyle = Style(textAlign: "center", padding: "5px 15px");
-  Style tableStyle = Style(borderCollapse: "collapse");
-  Style bodyRowStyle = Style(border: "1px solid #54585d", padding: "1px");
   List<Person> persons = getPeople();
   RootPage page = basePage([
-    H1(text: "overview", style: Style(fontSize: "24px")),
-    Table(style: tableStyle, rows: [
-      TableHeader(children: [
-        TableRow(tableData: [
-          TableData(text: "name", style: headerStyle),
-          TableData(text: "workout", style: headerStyle),
-          TableData(text: "fine/euro", style: headerStyle)
-        ])
-      ]),
-      ...persons
-          .map((Person p) => TableRow(style: bodyRowStyle, tableData: [
-                TableData(text: p.name, style: dataStyle),
-                TableData(text: p.checked ? "done" : " ", style: checkedStyle),
-                TableData(text: "${p.currentDelays}", style: dataStyle),
-              ]))
-          .toList(),
-    ])
+    Component.fromHtmlString("""
+      <h1>overview</h1>
+      <table class="table" style="max-width: 1000px; margin: auto;">
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>workout</th>
+            <th>fine/euro</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${renderMany(persons.map((p) => Component.fromHtmlString("""
+            <tr>
+              <td>${p.name}</td>
+              <td>${p.checked ? "done" : ""}</td>
+              <td>${p.currentDelays}</td>
+            </tr>
+          """)).toList())}
+        </tbody>
+      </table
+    """)
   ]);
   okHtmlResponse(response, page);
 }
