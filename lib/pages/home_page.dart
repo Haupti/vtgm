dcf21e1b1e624437cffcd9465b2a8e418b0eaf85
Marwin Acker <marwin.extern@funk.net>
2024-02-13 16:43:29 +0100

    make manager form prettier

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,25 +8,27 @@ void homePage(SsrRequest request, SsrResponse response) {
   List<Person> persons = getPeople();
   RootPage page = basePage([
     Component.fromHtmlString("""
-      <h1>overview</h1>
-      <table class="table" style="max-width: 1000px; margin: auto;">
-        <thead>
-          <tr>
-            <th>name</th>
-            <th>workout</th>
-            <th>fine/euro</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${renderMany(persons.map((p) => Component.fromHtmlString("""
+      <div style="max-width: 1000px; margin: auto;">
+        <h1>Overview</h1>
+        <table class="table">
+          <thead>
             <tr>
-              <td>${p.name}</td>
-              <td>${p.checked ? "done" : ""}</td>
-              <td>${p.currentDelays}</td>
+              <th>name</th>
+              <th>workout</th>
+              <th>fine/euro</th>
             </tr>
-          """)).toList())}
-        </tbody>
-      </table
+          </thead>
+          <tbody>
+            ${renderMany(persons.map((p) => Component.fromHtmlString("""
+              <tr>
+                <td>${p.name}</td>
+                <td>${p.checked ? "done" : ""}</td>
+                <td>${p.currentDelays}</td>
+              </tr>
+            """)).toList())}
+          </tbody>
+        </table
+      </div>
     """)
   ]);
   okHtmlResponse(response, page);
